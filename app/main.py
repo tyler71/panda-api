bd6348b5e2c4e3405106782ba2361d0a0805fdb6
TylerY <sagsdf_github_tyler71@xyzz.work>
2022-10-25 11:10:34 -0700

    junk files

--- app/main.py
+++ app/main.py
@@ -1,39 +1,10 @@
-from typing import Union
-
-from fastapi import FastAPI
-from pydantic import BaseModel
-from starlette.responses import JSONResponse
-
-app = FastAPI()
+from starlite import Starlite, get
 
 
-class Message(BaseModel):
-    message: str
+@get("/")
+def hello_world() -> dict[str, str]:
+    """Handler function that returns a greeting dictionary."""
+    return {"hello": "world"}
 
 
-class Item(BaseModel):
-    name: str
-    price: float
-    is_offer: Union[bool, None] = None
-
-
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
-
-
-@app.get("/items/{item_id}", response_model=Item, responses={404: {"model": Message}})
-async def read_item(item_id: str):
-    if item_id == "foo":
-        return JSONResponse(status_code=200, content={"id": "foo", "value": "there goes my hero"})
-    return JSONResponse(status_code=404, content={"message": "Item not found"})
-
-@app.get("/items2/{item_id}")
-async def read_item(item_id: str):
-    if item_id == "foo":
-        return {"id": "foo", "value": "there goes my bero"}
-
-
-@app.put("/items/{item_id}")
-def update_item(item_id: int, item: Item):
-    return {"item_name": item.name, "item_id": item_id}
+app = Starlite(route_handlers=[hello_world])
