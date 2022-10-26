d5276c50d47809e57aa4adaae10bcf62337fe91f
TylerY <sagsdf_github_tyler71@xyzz.work>
2022-10-26 07:06:05 -0700

    extra

--- app/main.py
+++ app/main.py
@@ -1,5 +1,8 @@
-from starlite import Starlite, get
+from starlite import Starlite, Router, get
+from pydantic import BaseModel
 
+class User(BaseModel):
+    Name: str
 
 @get("/")
 def hello_world() -> dict[str, str]:
@@ -7,6 +10,11 @@ def hello_world() -> dict[str, str]:
     a = 5 + 2
     return {"hello": "world"}
 
+@get("/{user_id:int}")
+def get_user(user_id: int) -> dict[str, str]:
+    return {str(user_id): f"hi {user_id}"}
 
-app = Starlite(route_handlers=[hello_world])
 
+user_router = Router(path="/user", route_handlers=[get_user])
+
+app = Starlite(route_handlers=[hello_world, user_router])
