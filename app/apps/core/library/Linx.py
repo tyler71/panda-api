cfa6daa4b6a4f4edae9b2901ea687ee3353f9de9
TylerY <sagsdf_github_tyler71@xyzz.work>
2022-11-04 08:20:42 -0700

    implement linx class

--- app/apps/core/library/Linx.py
+++ app/apps/core/library/Linx.py
@@ -1,126 +1,81 @@
-import datetime
-import hashlib
-import urllib.parse
+import io
 
 import requests
 
 
-class Yourls:
+class Skel:
+    request: dict
+    headers: dict
+    endpoint: str
+    method: str
+
+
+class Linx:
     def __init__(self, domain: str, apikey: str):
         self.domain = domain
         self.apikey = apikey
 
-    def _get_skel(self) -> dict:
+    def _get_skel(self) -> Skel:
         # Build the skeleton request
-        skel = {
-            'format': self.output,
-        }
-        skel.update(self._sig_timestamp())
+        skel = Skel()
+        skel.headers.update({
+            'Linx-Api-Key': self.apikey,
+            'Accept': 'application,json',
+        })
+
         return skel
 
-    def _make_request(self, request: dict) -> requests.models.Response:
+    def _make_request(self, req: Skel) -> requests.models.Response:
         # cleanup, encode, and complete the request
-        cleaned_request = {k: v for (k, v) in request.items() if v is not None}
-        if self.method == 'GET':
-            req = urllib.parse.urlencode(cleaned_request)
-            result = requests.get(f'{self.domain}?{req}')
-        elif self.method == 'POST':
-            result = requests.post(self.domain, data=cleaned_request)
+        cleaned_request = {k: v for (k, v) in req.request.items() if v is not None}
+        cleaned_headers = {k: v for (k, v) in req.headers.items() if v is not None}
+        if req.method == 'GET':
+            result = requests.get(f'{self.domain}{req.endpoint}{cleaned_request}', headers=cleaned_headers)
+        elif req.method == 'POST':
+            result = requests.post(f'{self.domain}{req.endpoint}', data=cleaned_request, headers=cleaned_headers)
+        elif req.method == 'PUT':
+            result = requests.put(f'{self.domain}{req.endpoint}', data=cleaned_request, headers=cleaned_headers)
+        elif req.method == 'DELETE':
+            result = requests.delete(f'{self.domain}{req.endpoint}', data=cleaned_request, headers=cleaned_headers)
         else:
             result = None
         return result
 
-    def version(self) -> requests.models.Response:
+    def upload(self, file: io.BytesIO, randomize_filename: bool = None, delete_key: str = None,
+               password: str = None, expiration_seconds: int = None):
         req = self._get_skel()
-        req.update({
-            'action': 'version',
+        req.headers.update({
+            'Linx-Randomize': randomize_filename,
+            'Linx-Delete-Key': delete_key,
+            'Linx-Access-Key': password,
+            'Linx-Expiry': expiration_seconds,
         })
+        req.method = 'PUT'
+        req.endpoint = '/upload'
+        req.request = file
         return self._make_request(req)
 
-    def shorten(self, url: str, title: str = None, keyword: str = None) -> requests.models.Response:
+    def overwrite(self, file: io.BytesIO, delete_key: str) -> requests.models.Response:
         req = self._get_skel()
-        req.update({
-            'action': 'shorturl',
-            'url': url,
-            'title': title,
-            'keyword': keyword,
+        req.headers.update({
+            'Linx-Delete-Key': delete_key,
         })
+        req.method = 'POST'
+        req.endpoint = '/upload'
+        req.request = file
         return self._make_request(req)
 
-    def expand(self, url: str) -> requests.models.Response:
+    def delete(self, filename: str, delete_key: str) -> requests.models.Response:
         req = self._get_skel()
-        req.update({
-            'action': 'expand',
-            'shorturl': url,
+        req.headers.update({
+            'Linx-Delete-Key': delete_key,
         })
+        req.method = 'DELETE'
+        req.endpoint = f'/{filename}'
         return self._make_request(req)
 
-    def stats(self, url: str) -> requests.models.Response:
+    def info(self, filename: str) -> requests.models.Response:
         req = self._get_skel()
-        req.update({
-            'action': 'url-stats',
-            'shorturl': url,
-        })
+        req.method = 'GET'
+        req.endpoint = f'/{filename}'
         return self._make_request(req)
-
-    def server_stats(self, filter: str = None) -> requests.models.Response:
-        req = self._get_skel()
-        valid_filters = ("top", "bottom", "rand", "last")
-        if filter is not None and filter not in valid_filters:
-            raise SyntaxWarning(f'Invalid argument. Available: {valid_filters}')
-        req.update({
-            'action': 'stats',
-            'filter': filter,
-        })
-        return self._make_request(req)
-
-    def db_stats(self) -> requests.models.Response:
-        req = self._get_skel()
-        req.update({
-            'action': 'db-stats',
-        })
-        return self._make_request(req)
-
-
-class YourlsUpdate(Yourls):
-    # https://github.com/timcrockford/yourls-api-edit-url
-    def __init__(self, domain: str, signature: str, *, method: str = "GET", output: str = 'json'):
-        super().__init__(domain, signature, method=method, output=output)
-
-    def update(self, shorturl: str, url: str, title: str = None) -> requests.models.Response:
-        req = self._get_skel()
-        req.update({
-            'action': 'update',
-            'shorturl': shorturl,
-            'url': url,
-            'title': title,
-        })
-        return self._make_request(req)
-
-    def geturl(self, url: str, *, exactly_once: bool = None) -> requests.models.Response:
-        req = self._get_skel()
-        req.update({
-            'action': 'geturl',
-            'url': url,
-            'exactly_once': exactly_once,
-        })
-        return self._make_request(req)
-
-
-class YourlsDelete(Yourls):
-    # https://github.com/claytondaley/yourls-api-delete
-    def __init__(self, domain: str, signature: str, *, method: str = "GET", output: str = 'json'):
-        super().__init__(domain, signature, method=method, output=output)
-
-    def delete(self, shorturl: str) -> requests.models.Response:
-        req = self._get_skel()
-        req.update({
-            'action': 'delete',
-            'shorturl': shorturl,
-        })
-        return self._make_request(req)
-
-
-class AllYourls(YourlsDelete, YourlsUpdate):
-    def __init__(self, domain: str, signature: str, *, method: str = "GET", output: str = 'json'):
-        super().__init__(domain, signature, method=method, output=output)
