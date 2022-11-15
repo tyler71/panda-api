df86294eb395c1ac148151f14cfcb23007e1aba7
TylerY <sagsdf_github_tyler71@xyzz.work>
2022-11-15 14:54:10 -0800

    Moving towards models

--- app/apps/image_overlay/models/qr.py
+++ app/apps/image_overlay/models/qr.py
@@ -17,13 +17,31 @@ class Size(BaseModel):
 
 
 class Qr:
-    def __init__(self, msg: str, options: dict, *, background_image_url=None, error='H'):
+    def __init__(self, msg: str, options: dict, *, format='png', background_image_url=None, error='H'):
         self.msg = msg
         self.options = options
         self.error = error
+        self.format = format
         self.background_image_url = background_image_url
         self._img = None
         self._msk = None
+        self._scale = None
+        self.QRCode = segno.make(msg, error=error)
+
+    @property
+    def scale(self) -> int:
+        if self._scale is None:
+            qr_data = io.BytesIO()
+            qrcode = self.QRCode
+            qrcode.save(qr_data, kind=self.format, scale=1)
+            qr = Image.open(qr_data)
+            qr_data.seek(0)
+            qr_data.truncate()
+
+            # Now we calculate the scale of the image. We will resize it as well, so we always go one scale up
+            qr_scale = math.ceil(self.size.width / qr.size[0])
+            self._scale = qr_scale
+        return qr_scale
 
     @property
     def mask(self) -> Image.Image:
@@ -32,26 +50,17 @@ class Qr:
     @property
     def img(self) -> Image.Image:
         if self._img is None:
-            # Here we are making the initial qr code image. We want the size of it
-            # so we can calculate the correct size later
             qr_data = io.BytesIO()
-            qrcode = segno.make(self.msg, error=self.error)
-            qrcode.save(qr_data, kind='png', scale=1)
-            qr = Image.open(qr_data)
-            qr_data.seek(0)
-            qr_data.truncate()
-
-            # Now we calculate the scale of the image. We will resize it as well, so we always go one scale up
-            qr_scale = math.ceil(self.size.width / qr.size[0])
             if self.background_image_url:
                 background_image = requests.get(self.background_image_url)
                 background_image_data = io.BytesIO(background_image.content)
-                qrcode.to_artistic(background=background_image_data, target=qr_data, kind='png', scale=qr_scale)
+                self.qrcode.to_artistic(background=background_image_data,
+                                        target=qr_data, kind=self.format, scale=self.scale)
             else:
-                qrcode.save(qr_data, kind='PNG', scale=qr_scale, **self.options)
+                self.qrcode.save(qr_data, kind=self.format, scale=self.scale, **self.options)
             qr = Image.open(qr_data)
             if qr.size != (self.size.width, self.size.height):
-                qr = qr.resize((self.size.width, self.size.height), Image.Resampling.LANCZOS)
+                qr = qr.resize(self.size.size, Image.Resampling.LANCZOS)
 
             self._img = qr
 
